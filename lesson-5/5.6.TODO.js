b90059048b88bd723896a53fb39be966bae849ca
wt88218 <163230869+wt88218@users.noreply.github.com>
2024-03-20 06:16:15 +0000

    5끝

--- lesson-5/5.6.TODO.js
+++ lesson-5/5.6.TODO.js
@@ -7,14 +7,41 @@
  */
 
 // 요청에 따른 라우트의 매핑 정의
-const routeRespMap;
-
+const routeRespMap = {
+    "/":"<h1>Home Page</h1><p>Welcome to my page!</P>",
+    "/about":"<h1>About Page</h1><p>About ME!</P>",
+    "/info":"<h1>Info Page</h1><p>Personal Info!</P>",
+    "/contact":"<h1>Contact Page</h1><p>Contact Me!</P>",
+};
 // listing5.5.js에서 (p. 96)
-const port;
+const port = 3000, 
+    http = require('http'),
+    httpStatus = require('http-status-codes'),
+    app = http.createServer();
 
-// 요청 라우트가 정의된 맵에 있는지 체크
+    app.on("request", (req, res) => {
+        res.writeHead(httpStatus.OK, {
+            "content-Type": "text/html",
+            
+        });
+        // 요청 라우트가 정의된 맵에 있는지 체크
+        if(routeRespMap[req.url]){ // "/about"
+            setTimeout(() => {
+                res.end(routeRespMap[req.url]);
+            }, 2000 ); // 2초
 
-// <<< 나머진 서버 코드 입력 하십시오 >>>
+        } else{
+            setTimeout(() =>{
+
+                res.end("<h1>404</h1><p>Where are you</p>");
+            }, 1000);
+        }
+    });
+if (process.env.NODE_ENV !== 'test'){
+    app.listen(port);
+    console.log(`Server at: http://localhost:${port}`);
+}
+
 
 // listing5.7.js에서 (p. 98)
 // 수동으로 응답에 지연을 걸기 위한 코드 감싸기
